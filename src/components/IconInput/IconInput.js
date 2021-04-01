3f1644136a1982f7379d4b7621a13a3cf46e13b5
Bastiaan van Graafeiland <bastiaan89@live.nl>
2021-04-01 09:34:23 +0200

    move styled components out of render function

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -22,44 +22,44 @@ const sizeVars = {
     '--padding': '8px',
   },
 };
+const Wrapper = styled.label`
+  display: block;
+  position: relative;
+  width: max-content;
+  color: ${COLORS.gray700};
+
+  &:hover,
+  &:focus-within {
+    color: ${COLORS.black};
+  }
+`;
+const Input = styled.input`
+  color: inherit;
+  font-size: var(--fontSize);
+  font-weight: 700;
+  width: var(--width);
+  height: var(--inputHeight);
+  padding: var(--padding);
+  padding-left: var(--inputHeight);
+  border: none;
+  border-bottom: calc(var(--borderWidth) * 1px) solid;
+  outline-offset: 2px;
+
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+`;
+const CustomIcon = styled(Icon)`
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+`;
 const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
-  const Wrapper = styled.label`
-    display: block;
-    position: relative;
-    width: max-content;
-    color: ${COLORS.gray700};
-
-    &:hover,
-    &:focus-within {
-      color: ${COLORS.black};
-    }
-  `;
-  const Input = styled.input`
-    color: inherit;
-    font-size: var(--fontSize);
-    font-weight: 700;
-    width: ${width}px;
-    height: var(--inputHeight);
-    padding: var(--padding);
-    padding-left: var(--inputHeight);
-    border: none;
-    border-bottom: calc(var(--borderWidth) * 1px) solid;
-    outline-offset: 2px;
-
-    &::placeholder {
-      font-weight: 400;
-      color: ${COLORS.gray500};
-    }
-  `;
-  const CustomIcon = styled(Icon)`
-    position: absolute;
-    left: 0;
-    top: 0;
-    bottom: 0;
-    margin: auto;
-  `;
   return (
-    <Wrapper style={sizeVars[size]}>
+    <Wrapper style={{ ...sizeVars[size], '--width': width + 'px' }}>
       <VisuallyHidden>{label}</VisuallyHidden>
       <CustomIcon
         id={icon}
